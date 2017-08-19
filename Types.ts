81d7a4f73a88bb7db79334e78ae7410b77bdcc88
Karl Xu <karl.yogi.xu@gmail.com>
2017-08-19 13:04:15 +0800

    Basic Types

---
+++ Types.ts
@@ -0,0 +1,29 @@
+// boolean
+let isDone: boolean = false
+
+// number
+let decimal: number = 6
+let hex: number = 0xf00d
+let binary: number = 0b1001
+let octal: number =0o744
+
+// sting
+let color: string = 'blue'
+color = 'red'
+
+let Fullname: string = `Bob bobington`
+let age: number = 37
+let sentence: string = `Hello, my name is ${Fullname}.
+
+I'll be ${age} next year`
+
+// array
+let list: number[] = [1, 2, 3]
+// let list: Array<number> = [1,2,3, 4]
+
+// Tuple
+let x: [string, number];
+x= ['hello', 10]
+//x= [ 10, 'hello']
+
+console.error(x)
